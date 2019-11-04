9b1f5233f4e2cb1d4dbcd5092a2967320a6f71f6
Michael Calkins <m.calkins576@gmail.com>
2019-11-04 20:31:01 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,9 @@
 def oxford_comma(array)
   output = ""
   if array.length == 1
-   output = array.join
+    output = array.join
+  elsif array.length == 2
+    output = array.join(" and ")
   else
     array.each do |item|
       output << "#{item}, " unless item == array[-1]
